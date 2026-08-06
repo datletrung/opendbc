f845dab9ab18a0cd2dfa5c103964af9a3f455c1b
Dat Le <letrungcaotung@gmail.com>
2026-08-06 18:05:10 -0300

    Toyota torque tuning changes

--- opendbc/car/toyota/values.py
+++ opendbc/car/toyota/values.py
@@ -17,8 +17,8 @@ PEDAL_TRANSITION = 10. * CV.MPH_TO_MS
 
 class CarControllerParams:
   STEER_STEP = 1
-  STEER_MAX = 1500
-  STEER_ERROR_MAX = 350     # max delta between torque cmd and torque motor
+  STEER_MAX = 2000          # default 1500
+  STEER_ERROR_MAX = 450     # default 350, max delta between torque cmd and torque motor
 
   # Lane Tracing Assist (LTA) control limits
   ANGLE_LIMITS: AngleSteeringLimits = AngleSteeringLimits(
@@ -43,11 +43,11 @@ class CarControllerParams:
     self.ACCEL_MIN = -3.5  # m/s2
 
     if CP.lateralTuning.which() == 'torque':
-      self.STEER_DELTA_UP = 15       # 1.0s time to peak torque
-      self.STEER_DELTA_DOWN = 25     # always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
+      self.STEER_DELTA_UP = 35       # default 15, 1.0s time to peak torque
+      self.STEER_DELTA_DOWN = 50     # default 25, always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
     else:
-      self.STEER_DELTA_UP = 10       # 1.5s time to peak torque
-      self.STEER_DELTA_DOWN = 25     # always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
+      self.STEER_DELTA_UP = 30       # default 10, 1.5s time to peak torque
+      self.STEER_DELTA_DOWN = 50     # default 25, always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
 
 
 class ToyotaSafetyFlags(IntFlag):
